3cd0f0ab6135d282e22f9ed2a5bb1ae6ee5faaae
Thanuj <thanujthankachan@gmail.com>
2026-07-17 14:40:21 +0530

    Delete 1.py

--- 1.py
+++
@@ -1,8 +0,0 @@
-a = int(input("Enter numerator: "))
-b = int(input("Enter denominator: "))
-
-if b == 0:
-    print("Error: Division by zero is not allowed.")
-else:
-    result = a / b
-    print("Result =", result)
